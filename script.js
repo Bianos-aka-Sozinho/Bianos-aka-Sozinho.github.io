5cd4bed0e815def2d5f8efc508c9de476e7a23c0
Bianos <bianossozinho@gmail.com>
2024-09-06 00:38:37 +0300

    Wooow

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', () => {
+   let instagram = document.getElementById('instagram');
+   let facebook = document.getElementById('facebook');
+   instagram.onclick = function () {
+
+   }
+   facebook.onclick = function () {
+    window.open('https://www.facebook.com/profile.php?id=100015265112652')
+   }
+});
